3590828400c2debb75c4a91a5ac9fe03426a2933
Abraham Velázquez <abraham.velazquez.cobos@gmail.com>
2021-09-15 18:24:34 -0500

    First commit. Adding README.txt file just to get started.

---
+++ wrangle-data.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+murders <- read_csv("data/murders.csv")
+murders <- murders %>%
+  mutate(region = factor(region), rate = total / population * 10 ^5)
+save(murders, file ="rda/murders.rda") # saves objects into rda file, rda stands for "R Data" another suffix could be .RData
